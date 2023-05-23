fc40f4b38c0cc80011f2c93831bfcc37a6ed7598
Duy <kduy410@gmail.com>
2023-05-24 02:21:42 +0700

    build todo app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setTodo(value);
+  }
+
+  const handleClick = (event) => {
+    setTodos(prev => [...prev, todo]);
+    setTodo("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" value={todo} onChange={handleChange}/>
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {todos.map(item => <li key={item}>{item}</li>)}
         </ul>
       </div>
     </div>
